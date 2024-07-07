4b3965ed88ccf1aee8c3254926c4b8833cae3a7b
Kyon Li <i@kyonli.com>
2024-07-07 11:21:04 +0800

    Initial release

---
+++ rootfs/build.sh
@@ -0,0 +1,55 @@
+#/bin/bash
+
+DIST=bookworm
+K_IMAGE_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/5.18.0-3/Image.gz"
+K_IMAGE_DEB_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/5.18.0-3/linux-image-5.18.0-handsomehack-g533401025051_5.18.0-handsomehack-g533401025051-1_arm64.deb"
+K_HEADERS_DEB_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/5.18.0-3/linux-headers-5.18.0-handsomehack-g533401025051_5.18.0-handsomehack-g533401025051-1_arm64.deb"
+
+if [ `id -u` -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+wget -P ../kernel "$K_IMAGE_URL"
+wget -P ../kernel "$K_IMAGE_DEB_URL"
+wget -P ../kernel "$K_HEADERS_DEB_URL"
+
+mkdir debian build
+debootstrap --arch=arm64 --foreign $DIST debian https://deb.debian.org/debian/
+LANG=C LANGUAGE=C LC_ALL=C chroot debian /debootstrap/debootstrap --second-stage
+cp ../deb-pkgs/*.deb ../kernel/*.deb chroot.sh debian/tmp/
+mount --bind /proc debian/proc
+mount --bind /dev debian/dev
+mount --bind /dev/pts debian/dev/pts
+mount --bind /sys debian/sys
+LANG=C LANGUAGE=C LC_ALL=C chroot debian /tmp/chroot.sh
+mv debian/tmp/info.md ./
+rm -rf debian/tmp/* debian/root/.bash_history > /dev/null 2>&1
+cp debian/etc/debian_version ./
+cp debian/boot/initrd.img* debian/usr/lib/linux-image*/qcom/*ufi003*.dtb ../kernel/
+umount debian/proc
+umount debian/dev/pts
+umount debian/dev
+umount debian/sys
+
+echo -e "\n\nNow you can make additional modifications to rootfs.\nPress ENTER to continue"
+head -n 1 >/dev/null
+
+#dd if=/dev/zero of=debian-ufi003.img bs=1M count=$(( $(df -m --output=used debian | tail -1 | awk '{print $1}') + 100 ))
+dd if=/dev/zero of=debian-ufi003.img bs=1M count=$(( $(du -ms debian | cut -f1) + 100 ))
+mkfs.ext4 -L rootfs debian-ufi003.img
+UUID=$(blkid -s UUID -o value debian-ufi003.img)
+cat <<EOF > debian/etc/fstab
+UUID=$UUID / ext4 defaults,noatime,commit=600,errors=remount-ro 0 1
+tmpfs /tmp tmpfs defaults,nosuid 0 0
+EOF
+mount debian-ufi003.img build
+rsync -aH debian/ build/
+umount build
+img2simg debian-ufi003.img rootfs.img
+xz rootfs.img
+rm -rf debian-ufi003.img debian build > /dev/null 2>&1
+
+cd ../kernel
+./build-boot-img.sh
+rm -rf initrd.img* *.dtb *.deb Image.gz > /dev/null 2>&1
---
+++ rootfs/chroot.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+LANG_TARGET=en_US.UTF-8
+PASSWORD=1234
+NAME=ufi003
+
+cat <<EOF > /etc/apt/sources.list
+deb http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware
+# deb-src http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware
+
+deb http://deb.debian.org/debian/ bookworm-updates main contrib non-free non-free-firmware
+# deb-src http://deb.debian.org/debian/ bookworm-updates main contrib non-free non-free-firmware
+
+deb http://deb.debian.org/debian/ bookworm-backports main contrib non-free non-free-firmware
+# deb-src http://deb.debian.org/debian/ bookworm-backports main contrib non-free non-free-firmware
+
+deb http://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
+# deb-src http://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
+EOF
+
+apt update
+apt dist-upgrade -y
+apt install -y locales network-manager initramfs-tools openssh-server chrony fake-hwclock zram-tools rmtfs qrtr-tools
+apt install -y /tmp/*.deb
+sed -i -e "s/# $LANG_TARGET UTF-8/$LANG_TARGET UTF-8/" /etc/locale.gen
+dpkg-reconfigure --frontend=noninteractive locales
+update-locale LANG=$LANG_TARGET LC_ALL=$LANG_TARGET LANGUAGE=$LANG_TARGET
+echo -n >/etc/resolv.conf
+echo -e "$PASSWORD\n$PASSWORD" | passwd
+echo $NAME > /etc/hostname
+sed -i "1a 127.0.0.1\t$NAME" /etc/hosts
+sed -i "s/::1\t\tlocalhost/::1\t\tlocalhost $NAME/g" /etc/hosts
+sed -i 's/^.\?PermitRootLogin.*$/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i 's/^.\?ALGO=.*$/ALGO=lzo-rle/g' /etc/default/zramswap
+sed -i 's/^.\?PERCENT=.*$/PERCENT=300/g' /etc/default/zramswap
+cat <<EOF > /tmp/info.md
+- 默认用户名：root
+- 默认密码：$PASSWORD
+- WiFi名称：openstick-failsafe
+- WiFi密码：12345678
+EOF
+rm -rf /etc/ssh/ssh_host_* /var/lib/apt/lists
+apt clean
+exit
